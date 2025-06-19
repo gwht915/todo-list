e61cc7e62b0387a86344be43f2b8e00521ead92c
gwht915 <gwht915@gmail.com>
2025-06-19 17:30:54 +0800

    router init

--- app.js
+++ app.js
@@ -8,6 +8,34 @@ app.get('/', (req, res) => {
   res.send('This is my todo list built with Express')
 })
 
+app.get('/todos', (req, res) => {
+  res.send('get all todos')
+})
+
+app.get('/todos/new', (req, res) => {
+  res.send('create todo')
+})
+
+app.post('/todos', (req, res) => {
+  res.send('add todo')
+})
+
+app.get('/todos/:id', (req, res) => {
+  res.send(`get todo: ${req.params.id}`)
+})
+
+app.get('/todos/:id/edit', (req, res) => {
+  res.send(`get todo edit: ${req.params.id}`)
+})
+
+app.put('/todos/:id', (req, res) => {
+  res.send('modify todo')
+})
+
+app.delete('/todos/:id', (req, res) => {
+  res.send('delete todo')
+})
+
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
 })
