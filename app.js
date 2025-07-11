21232200738237ebd68a453049345fdaa5c9be3d
gwht915 <gwht915@gmail.com>
2025-07-11 15:38:52 +0800

    feat: create todo function

--- app.js
+++ app.js
@@ -14,10 +14,17 @@ app.set('view engine', 'hbs')
 const Todo = require('./models/todo') // 載入 Todo model
 
 const mongoose = require('mongoose'); // 載入 mongoose
+
+// 引用 body - parser
+const bodyParser = require('body-parser')
+
 //const todo = require('./models/todo');
 
 //mongoose.connect(process.env.MONGODB_URI) // 設定連線到 mongoDB
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // 取得資料庫連線狀態
 const db = mongoose.connection
@@ -44,16 +51,21 @@ app.get('/', (req, res) => {
     .catch(error => console.error(error)) // 錯誤處理
 })
 
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
 app.get('/todos', (req, res) => {
   res.send('get all todos')
 })
 
-app.get('/todos/new', (req, res) => {
-  res.send('create todo')
-})
-
 app.post('/todos', (req, res) => {
-  res.send('add todo')
+  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
+  //return Todo.create({ name })     // 存入資料庫 (first method)
+  const todo = new Todo({name})
+  return todo.save()
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
 })
 
 app.get('/todos/:id', (req, res) => {
