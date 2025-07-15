a7508dab2a1bb5b6a0cc09e735554eb3d42a5015
gwht915 <gwht915@gmail.com>
2025-07-15 16:45:51 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ app.get('/', (req, res) => {
   //res.render('index')
   Todo.find() // 取出 Todo model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .sort({ name: 'asc' }) // 新增這裡：根據 _id 升冪排序
     .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
 })
@@ -136,7 +137,7 @@ app.post('/todos/:id/delete', (req, res) => {
   const id = req.params.id
   console.log(`id 111 = ${id}`)
   return Todo.findById(id)
-    .then(todo => todo.deleteOne())
+    .then(todo => todo.deleteOne())    //updated in version Mongoose@7.0 or over
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
