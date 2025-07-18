ba6da97c02ab44ca700c25d0b2566d427f5482c7
gwht915 <gwht915@gmail.com>
2025-07-18 16:53:26 +0800

    feat: modify detail page ui

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ const todos = require('./modules/todos')
 // 將網址結構符合 /todos 字串開頭的 request 導向 todos 模組 
 router.use('/todos', todos)
 
-console.log(`new index--->`)
+//console.log(`new index--->`)
 // 準備引入路由模組
 // 匯出路由器
 module.exports = router
